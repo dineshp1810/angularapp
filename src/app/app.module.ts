cae8511736173b4595cb3314dbae3352b9854470
dinesh_babu <dineshbabup2002@gmail.com>
2023-06-08 14:53:12 +0000

    Add login and signup component/routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { AdminModule } from './admin/admin.module';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule,
     UserModule,
     AdminModule
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
